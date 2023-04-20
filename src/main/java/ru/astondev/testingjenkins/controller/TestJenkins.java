bd211360ee1bf12a498df1456bd8ce99f5d88601
kovty <kovtynov.vladimir@gmail.com>
2023-04-20 17:09:47 +0300

    add

--- src/main/java/ru/astondev/testingjenkins/controller/TestJenkins.java
+++ src/main/java/ru/astondev/testingjenkins/controller/TestJenkins.java
@@ -1,2 +1,17 @@
-package ru.astondev.testingjenkins.controller;public class TestJenkins {
+package ru.astondev.testingjenkins.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestJenkins {
+
+    @GetMapping("/test1")
+    public String show1() {
+        return "TEST1";
+    }
+    @GetMapping("/test2")
+    public String show2() {
+        return "TEST2";
+    }
 }
